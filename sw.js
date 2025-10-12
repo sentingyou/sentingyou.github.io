e8dc04199c61144b0b56b934eefa4535eeaa310f
sentingyou <sentingyou@gmail.com>
2025-10-12 19:09:47 +0800

    Site updated: 2025-10-12 19:09:47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760267086803";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760267377453";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
